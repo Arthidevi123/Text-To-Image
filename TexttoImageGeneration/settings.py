0615dfefbb482bec4fd0904776ecb44bdfacf565
Your Name <you@example.com>
2026-04-07 21:42:19 +0530

    Fix WhiteNoise MissingFileError by disabling strict manifest check

--- TexttoImageGeneration/settings.py
+++ TexttoImageGeneration/settings.py
@@ -120,6 +120,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Storage backend for static files with compression and caching
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+WHITENOISE_MANIFEST_STRICT = False
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
